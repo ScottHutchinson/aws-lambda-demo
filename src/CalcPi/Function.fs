20eefa8eb32f223f872ae4e8963b5f4bf66fb654
Scott Hutchinson <scotthutchinson@usa.net>
2019-05-28 17:57:05 -0700

    Adding Monte Carlo algorithm.

--- src/CalcPi/Function.fs
+++ src/CalcPi/Function.fs
@@ -1,10 +1,41 @@
 namespace CalcPi
+open System
+
+module calcPi =
+
+    /// Calculates whether a point is within the unit circle
+    let insideCircle (x:float, y:float) =
+        if x * x + y * y <= 0.25 then 1 else 0
+
+    /// Generates random X, Y values between 0 and 1
+    let randomPoints max = seq {
+        let rnd = new Random()
+        for _ in 0 .. max do
+            yield 0.5 - rnd.NextDouble(), 0.5 - rnd.NextDouble() }
+
+    /// Generate specified number of random points and
+    /// calculate PI using Monte Carlo simulation
+    let monteCarloPI size =
+        let inside =
+            let points = randomPoints size
+            points |> Seq.sumBy insideCircle
+
+        // Estimate the value of PI
+        float inside / float size * 4.0
+
+    // Test the Monte Carlo PI calculation
+    // let pi = monteCarloPI 1000000
+
+    // Run the calculation 10 times and calculate average
+    // Change to 'Array.Parallel.map' to parallelize!
+    let averagePi points iterations =
+        [| for _ in 0 .. iterations -> points |]
+        |> Array.Parallel.map monteCarloPI
+        |> Array.average
 
 
 open Amazon.Lambda.Core
-
-open System
-
+open calcPi
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.Json.JsonSerializer>)>]
@@ -19,4 +50,4 @@ type Function() =
     /// <param name="context"></param>
     /// <returns></returns>
     member __.FunctionHandler (iterations: int) (_: ILambdaContext) =
-        3.14 + 0.001 * float iterations
+        averagePi 1000000 iterations
